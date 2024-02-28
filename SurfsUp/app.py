8fc338fbf527d7ed53c115640412e973bc845ab2
tokarczykbl <tokarczykbl@users.noreply.github.com>
2024-02-28 16:23:26 -0500

    Updating Flask portion of homework

---
+++ SurfsUp/app.py
@@ -0,0 +1,49 @@
+# Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+
+# Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Declare a Base using `automap_base()`
+Base = automap_base()
+
+# Use the Base class to reflect the database tables
+Base.prepare(autoload_with=engine)
+
+# Assign the measurement class to a variable called `Measurement` and
+# the station class to a variable called `Station`
+Measurement = Base.classes.measurement
+Staion = Base.classes.station
+
+# Create a session
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
